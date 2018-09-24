4a04aab3169609230875ff3f1ca4ff8400b47bdc
Javier Nualart Lee <javier@plc.ninja>
2018-09-24 14:52:47 -0700

    Solution3 almost ready for testing.
    On branch master
    Changes committed:
    
    	modified:   solution3.py

--- problem217/solution3.py
+++ problem217/solution3.py
@@ -1,55 +1,83 @@
 #!/usr/bin/env python3
 
-
-sum = 3
-digits= 3
-m = 10
-num = 0
-
-t_d_s = [
-    [0],
-    [1, 10],
-    [2, 11, 20],
-    [3, 12, 21, 30],
-    [4, 13, 22, 31, 40],
-    [5, 14, 23, 32, 41, 50],
-    [6, 15, 24, 33, 42, 51, 60],
-    [7, 16, 25, 34, 43, 52, 61, 70],
-    [8, 17, 26, 35, 44, 53, 62, 71, 80],
-    [9, 18, 27, 36, 45, 54, 63, 72, 81, 90],
-    [19, 28, 37, 46, 55, 64, 73, 82, 91],
-    [29, 38, 47, 56, 65, 74, 83, 92],
-    [39, 48, 57, 66, 75, 84, 93],
-    [49, 58, 67, 76, 85, 94],
-    [59, 68, 77, 86, 95],
-    [69, 78, 87, 96],
-    [79, 88, 97],
-    [89, 98],
-    [99],
-]
+from datetime import datetime
 
 
-def two_digit_sum(sum, m, adder=0):
-    #print("two digits: {0} {1} {2}".format(sum, m, adder))
+def get_two_digit_sum_table(m):
+    """
+    For each possible digit sum value:
+    Return the list of numbers (up to two digits) whose digit sum equals a given value.
+
+    :param m: Base number system
+    :return: A list of lists where each sublist contains all the numbers whose digit sum equals
+    the index of the sublist
+    """
+    table = []
+    for i in range(2 * m - 1):
+        table.append([number for number in two_digit_sum_generator(i, m)])
+    return table
+
+
+def two_digit_sum_generator(sum, m):
+    """
+    Return all the numbers (up to 2 digits) whose digit sum equals the given value.
+
+    :param sum: Value of the sum of the number's digits
+    :param m: Base number system
+    :return: The next number in increasing order
+    """
     if sum >= m:
         number = (sum - (m - 1)) * m + (m - 1)
     else:
         number = sum
     for _ in range(m - abs(m - sum - 1)):
-        yield number + adder
+        yield number
         number += (m - 1)
 
 
-def x_digit_sum(sum, m, x, adder=0):
+def two_digit_sum_lookup(sum, two_digit_sum_table, adder=0):
+    """
+    Look up all the numbers (up to 2 digits) whose digit sum equals the given value.
 
-    # If more than 2 digits
+    :param sum: Value of the sum of the number's digits
+    :param two_digit_sum_table: A table with the lists of all posible two digit sums (see get_two_digit_sum_table)
+    :param adder: And additional value to add to each returned number
+    :return: The next number in increasing order
+    """
+    for num in two_digit_sum_table[sum]:
+        yield num + adder
+
+
+def x_digit_sum_generator(sum, m, two_digit_sum_table, x, adder=0):
+    """
+    Return all the numbers (up to x digits) whose digit sum equals the given value.
+
+    :param sum: Value of the sum of the number's digits
+    :param m: Base number system
+    :param two_digit_sum_table: A table with the lists of all posible two digit sums (see get_two_digit_sum_table)
+    :param x: Number of digits
+    :param adder: And additional value to add to each returned number
+    :return: The next number in increasing order
+    """
+
+    # If more than 2 digits:
     if x > 2:
 
-        for i in range(m if sum > (m - 1) else sum + 1):
-            yield from x_digit_sum(sum - i, m,  x - 1, adder + i * m ** (x-1))
+        # Minimum (start) value for this digit must be large enough to reach the digit sum
+        start = 0
+        required_digits = sum // (m - 1)
+        if required_digits == x:
+            start = m - 1
+        if required_digits == x - 1:
+            start = sum % (m - 1)
 
+        # Reduce and recurse until only two digits are left
+        for i in range(start, m if sum > (m - 1) else sum + 1):
+            yield from x_digit_sum_generator(sum - i, m, two_digit_sum_table,  x - 1, adder + i * m ** (x-1))
+
+    # If only two digits: return from two_digit_sum
     else:
-        yield from two_digit_sum(sum, m, adder)
+        yield from two_digit_sum_lookup(sum, two_digit_sum_table, adder)
 
 
 def get_digit_sum(number, base):
@@ -65,24 +93,26 @@ def get_digit_sum(number, base):
 
 def balanced_number_generator(m):
 
-    # one digit
+    # Get the table with the two-digit sums:
+    tds_table = get_two_digit_sum_table(m)
+
+    # One digit numbers
     for i in range(1, m):
         yield i
 
-    # two digits
+    # Two digit numbers
     for i in range(1, m):
         yield i*m + i
 
-    # three digits
+    # Three digit numbers
     for i in range(1, m):
         for j in range(m):
             yield i*m**2 + j*m + i
 
-    # more digits
+    # Larger numbers
     digits = 4
     while True:
-        print("{} digit values:\n\n".format(digits))
-        import pdb;pdb.set_trace()
+        #print("{} digit values:\n\n".format(digits))
         half = digits // 2
         odd = digits % 2
 
@@ -100,7 +130,7 @@ def balanced_number_generator(m):
                 for j in range(m):
 
                     # For each right hand side equalling the digit sum
-                    for right in x_digit_sum(digit_sum, m, half):
+                    for right in x_digit_sum_generator(digit_sum, m, tds_table, half):
                         yield left * m ** (half + 1) + j * m ** half + right
 
         # If the number of total digits is even
@@ -114,20 +144,18 @@ def balanced_number_generator(m):
                 #print("digit sum: {}".format(digit_sum))
 
                 # For each right hand side equalling the digit sum
-                for right in x_digit_sum(digit_sum, m, half):
+                for right in x_digit_sum_generator(digit_sum, m, tds_table, half):
                     yield left * m ** half + right
 
         digits += 1
 
 
-
-prev = 0
+maxim = 10 ** 8
+start = datetime.now()
 for i in balanced_number_generator(10):
-    print(i)
-    if i <= prev:
-        print('          smaller')
+    if i > maxim:
         break
-    prev = i
+print(datetime.now() - start)
 
 #for j in range(20):
 #    print([i for i in two_digit_sum(j, 11)])
